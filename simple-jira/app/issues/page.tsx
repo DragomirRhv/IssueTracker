0b8354520de17b75c459ec91fa8431d7bcddfc5a
Dragomir Ryahov <dragomir.ryahov@gmail.com>
2023-10-10 23:27:47 +0300

    added navbar, eslint and prettier styling to the project

---
+++ simple-jira/app/issues/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const IssuesPage = (): React.JSX.Element => {
+  return <div>IssuesPage</div>;
+};
+
+export default IssuesPage;
--- simple-jira/app/page.tsx
+++ simple-jira/app/page.tsx
@@ -1,6 +1,3 @@
-
-export default function Home() {
-  return (
-    <div>Hello World!</div>
-  )
+export default function Home(): JSX.Element {
+  return <div>Hello World!</div>;
 }
